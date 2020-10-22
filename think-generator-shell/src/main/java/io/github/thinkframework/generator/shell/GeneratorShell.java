02904cc577f5626347ad4b2d57430eb37982a5f0
hdhxby <hdhxby@qq.com>
2020-10-22 09:28:56 +0800

    shell

--- think-generator-shell/src/main/java/io/github/thinkframework/generator/shell/GeneratorShell.java
+++ think-generator-shell/src/main/java/io/github/thinkframework/generator/shell/GeneratorShell.java
@@ -2,6 +2,8 @@ package io.github.thinkframework.generator.shell;
 
 import io.github.thinkframework.generator.core.Generator;
 import io.github.thinkframework.generator.core.annotation.GeneratorImportBeanDefinitionRegistrar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.shell.Availability;
