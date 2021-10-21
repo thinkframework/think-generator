0418bfd79818b3d7cbf746b7fd0c9a32277bcc9c
lixb-e <lixb-e@glodon.com>
2021-10-21 15:00:10 +0800

    目录结构

--- think-generator-boot-starter/src/main/java/io/github/thinkframework/boot/EnableGenerator.java
+++ think-generator-boot-starter/src/main/java/io/github/thinkframework/boot/EnableGenerator.java
@@ -1,5 +1,6 @@
-package io.github.thinkframework.generator.boot;
+package io.github.thinkframework.boot;
 
+import io.github.thinkframework.generator.boot.GeneratorImportBeanDefinitionRegistrar;
 import io.github.thinkframework.generator.boot.context.properties.GeneratorProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
--- think-generator-core/src/main/java/io/github/thinkframework/generator/baens/factory/xml/package-info.java
+++ think-generator-core/src/main/java/io/github/thinkframework/generator/baens/factory/xml/package-info.java
@@ -4,5 +4,6 @@
  *
  * @author hdhxby
  * @see org.springframework.beans.factory.xml.NamespaceHandler
+ * @since 1.0.0
  */
 package io.github.thinkframework.generator.baens.factory.xml;
